a5a80742503f1711c085bcfc514254e5f3d266d4
Kaya3842 <haichiendhnn1@gmail.com>
2024-06-26 10:52:56 +0700

    Update security rules

--- crm/models/crm_customer_request.py
+++ crm/models/crm_customer_request.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 class CustomerRequest(models.Model):
     _name = "crm.customer.request"
@@ -12,3 +13,23 @@ class CustomerRequest(models.Model):
     date = fields.Date(string="Date", required = 1, default=fields.Date.today())
     description = fields.Text()
     qty = fields.Float(string="Quantity", default=1)
+
+    # Methods
+    @api.model
+    def create(self, vals):
+        opportunity = self.env['crm.lead'].browse(vals.get('opportunity_id'))
+        if opportunity.stage_id and opportunity.stage_id.name != 'New':
+            raise ValidationError("Only create, update, delete on 'new' state of Opportunity")
+        return super(CustomerRequest, self).create(vals)
+
+    def write(self, vals):
+        for request in self:
+            if request.opportunity_id.stage_id and request.opportunity_id.stage_id.name != 'New':
+                raise ValidationError("Only create, update, delete on 'new' state of Opportunity")
+        return super(CustomerRequest, self).write(vals)
+
+    def unlink(self):
+        for request in self:
+            if request.opportunity_id.stage_id and request.opportunity_id.stage_id.name != 'New':
+                raise ValidationError("Only create, update, delete on 'new' state of Opportunity")
+        return super(CustomerRequest, self).unlink()
